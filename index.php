a2a4ffab596cb87d9bf9b2edfc3aa26206124d1e
Y-Junnya <30999279+Y-Junnya@users.noreply.github.com>
2017-09-23 16:27:30 +0900

    3.3.3 位置情報 戻し

--- index.php
+++ index.php
@@ -16,10 +16,10 @@ foreach ($events as $event) {
   //replyTextMessage($bot, $event->getReplyToken(), 'TextMessage');
   
   //‰æ‘œ•ÔM
-  replyImageMessage($bot, $event->getReplyToken(), 'https://' . $_SERVER['HTTP_HOST'] . '/imgs/original.jpg', 'https://' . $_SERVER['HTTP_HOST'] . '/imgs/preview.jpg');
+  //replyImageMessage($bot, $event->getReplyToken(), 'https://' . $_SERVER['HTTP_HOST'] . '/imgs/original.jpg', 'https://' . $_SERVER['HTTP_HOST'] . '/imgs/preview.jpg');
   
   //ˆÊ’uî•ñ‚ð•ÔM
-  //replyLocationMessage($bot, $event->getReplyToken(), 'LINE', '“Œ‹ž“sa’J‹æa’J2-21-1 ƒqƒJƒŠƒG27ŠK', '35.659025', '139.703473');
+  replyLocationMessage($bot, $event->getReplyToken(), 'LINE', '“Œ‹ž“sa’J‹æa’J2-21-1 ƒqƒJƒŠƒG27ŠK', '35.659025', '139.703473');
   }
 
 function replyTextMessage($bot, $replyToken, $text) {
@@ -44,7 +44,7 @@ function replyLocationMessage($bot, $replyToken, $title, $address, $lat, $lon) {
 }
 
 
-//$inputString = file_get_contents('php://input');
-//error_log($inputString);
+$inputString = file_get_contents('php://input');
+error_log($inputString);
 
 ?>
